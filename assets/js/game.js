d0823cef300eeed67f966a57eb9a95017964b505
brimacx3 <bemckinney0813@gmail.com>
2020-06-12 16:38:39 -0400

    update console

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,13 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+var name = "your name";
+console.log(name);
+
+console.log(playerName);
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
